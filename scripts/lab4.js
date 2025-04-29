13fd3d6ebbd97b54800f2403ae731a70e989c652
asvo <asvo@ucsd.edu>
2025-04-29 09:54:08 -0700

    Fix sumValues

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -8,14 +8,14 @@
 function sumValues(num1, num2, add) {
     // This is a comment for lab 4.
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return add;
     }
 }
 
